80204f812333194dcdbf17794b541c3d9d30b281
Leo GERBERON <leo.gerberon@equasens.com>
2023-07-12 09:10:09 +0200

    nitial commit 2

---
+++ client/app.js
@@ -0,0 +1,35 @@
+const EventSource = require('eventsource');
+const axios = require('axios');
+
+// Adresse IP et port du serveur SSE
+const sseServerAddress = 'http://hook-connect.ruokki.ovh/connect';
+
+// Adresse IP et port de destination des requêtes redirigées
+const destinationAddress = 'http://localhost:8080'; // Exemple : localhost:8000
+
+// Établir une connexion SSE avec le serveur
+const eventSource = new EventSource(sseServerAddress);
+
+// Écouter les événements SSE
+eventSource.onmessage = (event) => {
+    const requestData = JSON.parse(event.data);
+    console.log("Redirection de la requete", requestData)
+    // Rediriger la requête vers l'adresse de destination spécifiée
+    axios({
+        method: requestData.method,
+        url: destinationAddress + requestData.path,
+        headers: requestData.headers,
+        data: requestData.body
+    })
+        .then((response) => {
+            console.log('Réponse reçue du serveur de destination:', response.data);
+        })
+        .catch((error) => {
+            console.error('Erreur lors de la redirection de la requête:', error);
+        });
+};
+
+// Gérer les erreurs de connexion SSE
+eventSource.onerror = (error) => {
+    console.error('Erreur de connexion SSE:', error);
+};
---
+++ server/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const http = require('http');
+const sse = require('sse');
+const url = require('url');
+
+const app = express();
+const client = express();
+const server = http.createServer(app);
+
+// Premier port avec endpoint SSE /connect
+const ssePort = 3000;
+const sseClients = [];
+
+client.get('/connect', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const client = new sse.Client(req, res);
+    sseClients.push(client);
+    let numberConections = sseClients.length;
+    console.log("Connexion au sse", numberConections)
+
+    req.on('close', () => {
+        console.log("Connexion au sse termine", numberConections)
+
+        const index = sseClients.indexOf(client);
+        if (index !== -1) {
+            sseClients.splice(index, 1);
+        }
+    });
+});
+
+// Deuxième port qui redirige les requêtes vers SSE
+const redirectPort = 4000;
+
+app.use((req, res) => {
+    const { headers, method, url: requestUrl, body } = req;
+
+    // Construire l'objet JSON avec les informations de la requête
+    const requestData = {
+        headers,
+        body,
+        path: requestUrl,
+        method: method
+    };
+
+    // Envoyer l'objet JSON à tous les clients SSE connectés
+    for (const client of sseClients) {
+        client.send(JSON.stringify(requestData));
+    }
+
+    res.sendStatus(200);
+});
+
+// Démarrer les serveurs
+client.listen(ssePort, () => {
+    console.log(`Serveur SSE écoutant sur le port ${ssePort}`);
+});
+
+server.listen(redirectPort, () => {
+    console.log(`Serveur de redirection écoutant sur le port ${redirectPort}`);
+});
